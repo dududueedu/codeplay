7fb68a8ac6c73bbcfd7db3f84c9053db468ca5fd
Eduardo Gomes <eduardooffside@gmail.com>
2022-03-29 00:31:34 -0300

    add: sysout-running server

--- codeplay.server/src/main/java/com/dev/drew/codeplay/server/CodeplayApplication.java
+++ codeplay.server/src/main/java/com/dev/drew/codeplay/server/CodeplayApplication.java
@@ -8,6 +8,7 @@ public class CodeplayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeplayApplication.class, args);
+		System.out.println("*** |codeplay - running server| ***");
 	}
 
 }
