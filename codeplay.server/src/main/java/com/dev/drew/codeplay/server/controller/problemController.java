ff7b54d493805d13274544943d30bda20cce4f1b
Eduardo Gomes <eduardooffside@gmail.com>
2022-04-07 16:28:13 -0300

    add: method-delete

--- codeplay.server/src/main/java/com/dev/drew/codeplay/server/controller/problemController.java
+++ codeplay.server/src/main/java/com/dev/drew/codeplay/server/controller/problemController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +33,16 @@ public class problemController {
 			return new ResponseEntity<problemEntity>(problem, HttpStatus.OK);
 		} catch (Exception error) {
 			return new ResponseEntity<>(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@DeleteMapping("/problem/{id}")
+	public ResponseEntity<?> deleteProblemById(@PathVariable("id") String id){
+		try {
+			problemRepo.deleteById(id);
+			return new ResponseEntity<>("Excluido com sucesso!", HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
 		}
 	}
 }
