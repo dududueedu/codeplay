7abb72659e1cb2f86fe0ac464f151189865c86a0
Eduardo Gomes <eduardooffside@gmail.com>
2022-05-18 02:46:23 -0300

    add settings and tests

--- codeplay.client/src/App.js
+++ codeplay.client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import InfoProblem from './components/InfoProblem'
+import ProblemCode from './components/ProblemCode';
 
 function App() {
   return (
     <div className="App">
         <InfoProblem />
+        <ProblemCode />
     </div>
   );
 }
---
+++ codeplay.client/src/components/ProblemCode/index.js
@@ -0,0 +1,32 @@
+function ProblemCode(props) {
+  const renderQuest = () => {
+    return (
+      <div className="text">
+        <div className="gridName box">
+          <h3 className="boxTitle"> Nome </h3>
+          <span className="information">nome do problema</span>
+        </div>
+        <div className="gridDesc box">
+          <h3 className="boxTitle"> Descrição </h3>
+          <span className="information">Descrição do problema</span>
+        </div>
+        <div className="gridInput box">
+          <h3 className="boxTitle"> Entrada </h3>
+          <span className="information">Entrada do problema</span>
+        </div>
+        <div className="gridOutput box">
+          <h3 className="boxTitle"> Saída </h3>
+          <span className="information">O que se espera</span>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="text-container">
+      <div className="text-box">{renderQuest()}</div>
+    </div>
+  );
+}
+
+export default ProblemCode;
