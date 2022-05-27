8f5f2d99362b5aaf6b9929a05f46bb46a5fcc7e5
Eduardo Gomes <eduardooffside@gmail.com>
2022-05-27 02:39:33 -0300

    add: info-problem

--- codeplay.client/src/components/InfoProblem/index.js
+++ codeplay.client/src/components/InfoProblem/index.js
@@ -1,12 +1,32 @@
 import "./index.css";
 
 function InfoProblem(props) {
+  const infoPlaceholder =
+    "Ao ler a entrada, use sempre input(), sem nenhum valor dentro dos parênteses. Exemplo: int(input()) para ler um número e convertê-lo para inteiro.\nSeu programa não deve conter acentos. Caso contrário, o sistema irá apontar erro em sua solução.\nAs saídas do seu programa devem seguir o padrão exibido em 'Saída'\n\nDivirta-se, bom jogo!";
+
   return (
-    <div className="one-container">
-      <div className="content-box">
-        <p>test problems</p>
+    <form id="form-problemcode">
+      <div className="problemcode-container">
+        <p className="text-problem"> CODEPLAY: </p>
+        <textarea
+          onChange={(e) => e}
+          type="textarea"
+          placeholder={infoPlaceholder}
+          rows="5"
+          cols="5"
+          className="textarea-problem"
+        />
+        <input
+          type="button"
+          onClick={(e) => e}
+          className="btn-problem"
+          value="Limpar"
+        />
+        <button type="submit" value="enviar" className="btn-problem">
+          Enviar
+        </button>
       </div>
-    </div>
+    </form>
   );
 }
 
