f79cd4b6e9fec82f27d56946600109d5f0705573
Eduardo Gomes <eduardooffside@gmail.com>
2022-06-08 22:48:20 -0300

    update-GamePlayer: 0

--- codeplay.client/src/resources/GamePlayer/index.js
+++ codeplay.client/src/resources/GamePlayer/index.js
@@ -5,7 +5,24 @@ function GamePlayer(pros) {
         return `-${pixel}px`;
     }
 
+    function getDirectPixel(direct) {
+        const SPRITE_SIZE = 20; // TESTING...
+        switch (direct) {
+            case 1:
+                return `${SPRITE_SIZE*0}px`
+            case 2:
+                return `-${SPRITE_SIZE*3}px`
+            case 3:
+                return `-${SPRITE_SIZE}px`
+            case 4:
+                return `-${SPRITE_SIZE*2}px`
+            default:
+                console.log('INVALID DIRECTING');
+        }
+    }
+
     getPixel();
+    getDirectPixel();
 }
 
 export default GamePlayer;
