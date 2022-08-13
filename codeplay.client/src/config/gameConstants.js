71eb7292c021561db57445891679b91dc5da70a0
Eduardo Gomes <eduardooffside@gmail.com>
2022-08-13 01:46:49 -0300

    add constants

--- codeplay.client/src/config/gameConstants.js
+++ codeplay.client/src/config/gameConstants.js
@@ -11,3 +11,6 @@ export const SPRITE_SIZE = 32;
 export const PRO = 1; 
 export const CLN = 2; // colision
 export const PCN = 3; // problem colision
+
+export const MAP_HEIGHT = 20 * SPRITE_SIZE
+export const MAP_WIDTH = 30 * SPRITE_SIZE
