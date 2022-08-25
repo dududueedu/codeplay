b825c85147397f215b1da66c6ab502df0174d299
Eduardo Gomes <eduardooffside@gmail.com>
2022-08-25 04:22:48 -0300

    stylizando_2

--- codeplay.client/src/components/InfoProblem/index.js
+++ codeplay.client/src/components/InfoProblem/index.js
@@ -5,19 +5,19 @@ function InfoProblem(props) {
     return (
       <div className="text">
         <div className="gridName box">
-          <h3 className="boxTitle"> Nome </h3>
+          <p className="boxTitle"> NOME </p>
           <span className="information">nome do problema</span>
         </div>
         <div className="gridDesc box">
-          <h3 className="boxTitle"> Descrição </h3>
+          <p className="boxTitle"> DESCRIÇÃO </p>
           <span className="information">Descrição do problema</span>
         </div>
         <div className="gridInput box">
-          <h3 className="boxTitle"> Entrada </h3>
+          <p className="boxTitle"> ENTRADA </p>
           <span className="information">Entrada do problema</span>
         </div>
         <div className="gridOutput box">
-          <h3 className="boxTitle"> Saída </h3>
+          <p className="boxTitle"> SAÍDA </p>
           <span className="information">O que se espera</span>
         </div>
       </div>
