d60961e9676b72846ff5e65d7649172991eb2b98
Eduardo Gomes <eduardooffside@gmail.com>
2022-09-14 01:39:03 -0300

    add functions game-map

--- codeplay.client/src/resources/GameMap/index.js
+++ codeplay.client/src/resources/GameMap/index.js
@@ -1,5 +1,47 @@
-import { MAP_HEIGHT, MAP_WIDTH } from "../../config/gameConstants";
+import {
+  SPRITE_SIZE,
+  MAP_HEIGHT,
+  MAP_WIDTH,
+  PCN,
+  PRO,
+  CLN,
+} from "../../config/gameConstants";
 import "./index.css";
+
+function MapTile(props) {
+  function getTileEvent(type) {
+    switch (type) {
+      case PRO:
+        return "quest";
+      case PCN:
+        return "quest_colision";
+      case CLN:
+        return "colision";
+      default:
+        break;
+    }
+  }
+
+  return (
+    <div
+      className={`tile ${getTileEvent(props.tile)}`}
+      style={{
+        height: SPRITE_SIZE,
+        width: SPRITE_SIZE,
+      }}
+    />
+  );
+}
+
+function MapRow(props) {
+  return (
+    <div style={{ height: `${SPRITE_SIZE}px` }}>
+      {props.tiles.map((tile, index) => {
+        return <MapTile tile={tile} key={index} />;
+      })}
+    </div>
+  );
+}
 
 function GameMap(props) {
   return (
@@ -18,3 +60,7 @@ function GameMap(props) {
 }
 
 export default GameMap;
+
+/*{props.tiles.map((row, index) => {
+  return <MapRow tiles={row} key={index} />;
+})}*/
