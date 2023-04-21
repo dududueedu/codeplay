e125c352f26c86d19f70b412b2860b2f518da950
Eduardo Gomes <eduardooffside@gmail.com>
2023-04-20 22:31:46 -0300

    update code clean

--- codeplay.client/src/components/ProblemCode/index.js
+++ codeplay.client/src/components/ProblemCode/index.js
@@ -5,33 +5,29 @@ import { connect } from 'react-redux'
 
 function ProblemCode(props) {
   const infoPlaceholder =
-    "Para fazer uma leitura do teclado, use sempre input(), sem nenhum valor nos parênteses.\nExemplo: int(input()) para ler um número e convertê-lo para inteiro.\n\nSeu programa não deve conter acentos.\nAs saídas do seu programa devem seguir o padrão exibido em 'Saída'.\n\nDivirta-se, bom jogo!";
+    "INSTRUÇÕES:\n- Use input(), sem nenhum valor entre os parênteses, para fazer uma leitura do teclado. Ex: int(input()) para ler um número e convertê-lo para inteiro.\n\n- Seu programa não deve conter acentos e as saídas devem seguir o padrão exibido em SAÍDA.";
 
-    const [code, setCode] = useState("")
+  const [code, setCode] = useState("");
+  const currentProblem = props.problemId;
 
-    const currentProblem = props.problemId;
-    //console.log("challenge", currentProblem);
+  async function handleCodeSubmission(e) {
+    e.preventDefault()
 
-    async function handleCodeSubmission(e) {
-      e.preventDefault()
+    const jsonData = {
+      codeInput: code,  
+      problem_id: currentProblem.id, 
+      language_id: null
+    }
 
-      const jsonData = {
-        codeInput: code,  
-        problem_id: currentProblem.id, 
-        language_id: null
-      }
-      //console.log("meu jSon: ", jsonData);
-
-      const response = await submission(jsonData);
-      console.log("meu response: ", response);
-      const submissionResult = response.data
-      console.log("submissionResult: ", submissionResult)
-      if (submissionResult.result) {
-          console.log(submissionResult);
-          alert("Sua solução foi aprovada! PARABÉNS!");
-      } else 
-          alert("Erro. Motivo:\n\n" + submissionResult.error);
-      
+    const response = await submission(jsonData);
+    console.log("meu response: ", response);
+    const submissionResult = response.data
+    console.log("submissionResult: ", submissionResult)
+    if (submissionResult.result) {
+        console.log(submissionResult);
+        alert("Sua solução foi aprovada! PARABÉNS!");
+    } else 
+        alert("Erro. Motivo:\n\n" + submissionResult.error);
   }
 
   return (
