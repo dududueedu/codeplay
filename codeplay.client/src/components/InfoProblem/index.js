36b466dd8595ce23c29ef4ac3244cfbe9a6753c6
Eduardo Gomes <eduardooffside@gmail.com>
2023-05-11 21:03:22 -0300

    update map-quizz_infoProblem

--- codeplay.client/src/components/InfoProblem/index.js
+++ codeplay.client/src/components/InfoProblem/index.js
@@ -6,6 +6,9 @@ import { dispatchProblem } from './dispatchProblem'
 import { EAST, QUEST, SOUTH, WEST, NORTH } from '../../config/gameConstants'
 import Swal from 'sweetalert2'
 import gameOver from '../../data/Map/game-over.png'
+import cans from '../../data/Quizz/cans.png'
+import Star from '../../data/Quizz/star.png'
+import Sad from '../../data/Quizz/sad.png'
 
 function InfoProblem(props) {
 
@@ -40,13 +43,8 @@ function InfoProblem(props) {
       (props.position[0] === 160 && props.position[1] === 128 && props.facing === NORTH)||
       (props.position[0] === 224 && props.position[1] === 96 && props.facing === WEST)||
       (props.position[0] === 192 && props.position[1] === 128 && props.facing === NORTH)||
-      (props.position[0] === 288 && props.position[1] === 320)||
       (props.position[0] === 672 && props.position[1] === 160)||
-      (props.position[0] === 352 && props.position[1] === 480)||
-      (props.position[0] === 832 && props.position[1] === 352)||
-      (props.position[0] === 832 && props.position[1] === 288)||
-      (props.position[0] === 800 && props.position[1] === 320)||
-      (props.position[0] === 864 && props.position[1] === 320)
+      (props.position[0] === 352 && props.position[1] === 480)
     ) {
         Swal.fire({
           imageUrl: gameOver,
@@ -70,6 +68,49 @@ function InfoProblem(props) {
           }
         });
       }
+      else if((props.position[0] === 288 && props.position[1] === 320)||
+              (props.position[0] === 832 && props.position[1] === 352)) {
+                Swal.fire({
+                  text: 'Quantos latas são necessárias para construir uma pilha de latas no formato acima que tenha a altura de cinco latas?',
+                  imageUrl: cans,
+                  imageWidth: 500,
+                  imageHeight: 200,
+                  imageAlt: 'CANS image',
+                  input: 'text',
+                  inputAttributes: {
+                    autocapitalize: 'off'
+                  },
+                  showCancelButton: false,
+                  confirmButtonText: 'ENVIAR',
+                  confirmButtonColor: '#98be23',
+                  showLoaderOnConfirm: true,
+                  preConfirm: (response) => {
+                    if(response === '15'){
+                      Swal.fire({
+                        imageUrl: Star,
+                        imageWidth: 75,
+                        imageHeight: 75,
+                        color: '#98be23',
+                        title: 'Parabéns! Você acertou.',
+                        confirmButtonColor: '#98be23',
+                        showCancelButton: false,
+                        confirmButtonText: 'Ok!',
+                        })
+                    }else{
+                      Swal.fire({
+                        imageUrl: Sad,
+                        imageWidth: 75,
+                        imageHeight: 75,
+                        color: '#98be23',
+                        title: 'Que pena! Não foi dessa vez.',
+                        confirmButtonColor: '#98be23',
+                        showCancelButton: false,
+                        confirmButtonText: 'Ok!',
+                      })
+                    }
+                  },
+                })
+              }
   }, [props])
 
   useEffect(() => {
