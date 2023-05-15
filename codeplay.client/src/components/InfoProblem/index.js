c94b799f62a38431731c2afdaea1e66736c0ab75
Eduardo Gomes <eduardooffside@gmail.com>
2023-05-14 23:49:48 -0300

    update mecanism-game-Map

--- codeplay.client/src/components/InfoProblem/index.js
+++ codeplay.client/src/components/InfoProblem/index.js
@@ -6,9 +6,8 @@ import { dispatchProblem } from './dispatchProblem'
 import { EAST, QUEST, SOUTH, WEST, NORTH } from '../../config/gameConstants'
 import Swal from 'sweetalert2'
 import gameOver from '../../data/Map/game-over.png'
-import cans from '../../data/Quizz/cans.png'
-import Star from '../../data/Quizz/star.png'
-import Sad from '../../data/Quizz/sad.png'
+import { ModalCans, ModalBook } from './modal'
+import { useNavigate } from 'react-router-dom'
 
 function InfoProblem(props) {
 
@@ -22,6 +21,7 @@ function InfoProblem(props) {
 
   const [currentQuest, setCurrentQuest] = useState(initialState);
   const [questList, setQuestList] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     listProblems().then(res => {
@@ -44,7 +44,7 @@ function InfoProblem(props) {
       (props.position[0] === 224 && props.position[1] === 96 && props.facing === WEST)||
       (props.position[0] === 192 && props.position[1] === 128 && props.facing === NORTH)||
       (props.position[0] === 672 && props.position[1] === 160)||
-      (props.position[0] === 352 && props.position[1] === 480)
+      (props.position[0] === 288 && props.position[1] === 320)
     ) {
         Swal.fire({
           imageUrl: gameOver,
@@ -65,53 +65,20 @@ function InfoProblem(props) {
             }, "3000");
           } else if (result.isDenied) {
             Swal.fire('Ok. Até mais!', 'Não desamine, estude e volte depois ;)', 'info');
+            setTimeout(() => {
+              navigate('/stop');
+              window.location.reload();
+            }, "3000");
           }
         });
       }
-      else if((props.position[0] === 288 && props.position[1] === 320)||
-              (props.position[0] === 832 && props.position[1] === 352)) {
-                Swal.fire({
-                  text: 'Quantos latas são necessárias para construir uma pilha de latas no formato acima que tenha a altura de cinco latas?',
-                  imageUrl: cans,
-                  imageWidth: 500,
-                  imageHeight: 200,
-                  imageAlt: 'CANS image',
-                  input: 'text',
-                  inputAttributes: {
-                    autocapitalize: 'off'
-                  },
-                  showCancelButton: false,
-                  confirmButtonText: 'ENVIAR',
-                  confirmButtonColor: '#98be23',
-                  showLoaderOnConfirm: true,
-                  preConfirm: (response) => {
-                    if(response === '15'){
-                      Swal.fire({
-                        imageUrl: Star,
-                        imageWidth: 75,
-                        imageHeight: 75,
-                        color: '#98be23',
-                        title: 'Parabéns! Você acertou.',
-                        confirmButtonColor: '#98be23',
-                        showCancelButton: false,
-                        confirmButtonText: 'Ok!',
-                        })
-                    }else{
-                      Swal.fire({
-                        imageUrl: Sad,
-                        imageWidth: 75,
-                        imageHeight: 75,
-                        color: '#98be23',
-                        title: 'Que pena! Não foi dessa vez.',
-                        confirmButtonColor: '#98be23',
-                        showCancelButton: false,
-                        confirmButtonText: 'Ok!',
-                      })
-                    }
-                  },
-                })
-              }
-  }, [props])
+      else if((props.position[0] === 320 && props.position[1] === 448)) {
+        ModalCans();
+      }
+      else if((props.position[0] === 832 && props.position[1] === 352)) {
+        ModalBook();
+      }
+  }, [props, navigate])
 
   useEffect(() => {
     let currentProblem = null;
---
+++ codeplay.client/src/components/InfoProblem/modal/index.js
@@ -0,0 +1,97 @@
+import Swal from 'sweetalert2'
+import cans from '../../../data/Quizz/cans.png'
+import Star from '../../../data/Quizz/star.png'
+import Sad from '../../../data/Quizz/sad.png'
+import book from '../../../data/Quizz/book.png'
+
+export function ModalCans(){
+    Swal.fire({
+        text: 'Quantos latas são necessárias para construir uma pilha de latas no formato acima que tenha a altura de cinco latas?',
+        imageUrl: cans,
+        imageWidth: 500,
+        imageHeight: 200,
+        imageAlt: 'CANS image',
+        input: 'text',
+        inputAttributes: {
+          autocapitalize: 'off'
+        },
+        showCancelButton: false,
+        confirmButtonText: 'ENVIAR',
+        confirmButtonColor: '#98be23',
+        showLoaderOnConfirm: true,
+        preConfirm: (response) => {
+          if(response === '15'){
+            Swal.fire({
+              imageUrl: Star,
+              imageWidth: 75,
+              imageHeight: 75,
+              color: '#98be23',
+              title: 'Parabéns! Você acertou.',
+              confirmButtonColor: '#98be23',
+              showCancelButton: false,
+              confirmButtonText: 'Ok!',
+              })
+          }else{
+            Swal.fire({
+              imageUrl: Sad,
+              imageWidth: 75,
+              imageHeight: 75,
+              color: '#98be23',
+              title: 'Que pena! Não foi dessa vez. Tente outra vez!',
+              confirmButtonColor: '#98be23',
+              showCancelButton: false,
+              confirmButtonText: 'Ok!',
+            })
+            setTimeout(() => {
+              window.location.reload();
+            }, "5000");
+          }
+        },
+    })
+}
+
+export function ModalBook(){
+    Swal.fire({
+        text: 'Maike com raiva da professora, descontou no livro de Lógica e rasgou as páginas 9, 10, 100, 111, 112. Quantas folhas ele rasgou?',
+        imageUrl: book,
+        imageWidth: 200,
+        imageHeight: 200,
+        imageAlt: 'BOOK image',
+        input: 'text',
+        inputAttributes: {
+          autocapitalize: 'off'
+        },
+        showCancelButton: false,
+        confirmButtonText: 'ENVIAR',
+        confirmButtonColor: '#98be23',
+        showLoaderOnConfirm: true,
+        preConfirm: (response) => {
+          if(response === '4'){
+            Swal.fire({
+              imageUrl: Star,
+              imageWidth: 75,
+              imageHeight: 75,
+              color: '#98be23',
+              title: 'Parabéns! Você acertou.',
+              confirmButtonColor: '#98be23',
+              showCancelButton: false,
+              confirmButtonText: 'Ok!',
+              })
+          }else{
+            Swal.fire({
+              imageUrl: Sad,
+              imageWidth: 75,
+              imageHeight: 75,
+              color: '#98be23',
+              title: 'Que pena! Não foi dessa vez. Tente outra vez!',
+              confirmButtonColor: '#98be23',
+              showCancelButton: false,
+              confirmButtonText: 'Ok!',
+            })
+            setTimeout(() => {
+              window.location.reload();
+            }, "5000");
+          }
+        },
+    })
+}
