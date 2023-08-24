d5f3727ea63187f2460425cc9c9942c9208686fd
Eduardo Gomes <eduardooffside@gmail.com>
2023-08-24 17:16:18 -0300

    add effect typewriter

--- codeplay.client/src/components/Home/index.js
+++ codeplay.client/src/components/Home/index.js
@@ -1,5 +1,6 @@
 import "./index.css"
 import { useNavigate } from 'react-router-dom' 
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
 
 
 function Home() {
@@ -9,10 +10,17 @@ function Home() {
         navigate('/quizz');
     }
 
+    const [typewriter] = useTypewriter({
+        words: ['Aqui você irá praticar programação de forma mais descontraída. Tome cuidado no caminho que irá trilhar e ao final, tente alcançar o objetivo do jogo.'],
+        loop: 1,
+        typeSpeed: 75
+    })
+
     return (
         <div className='container_home'>
             <div className='space_home'>
                 <div className="text-home">BEM VINDO AO CODEPLAY</div>
+                <span className="typewriter-home">{typewriter}<Cursor cursorColor='black'/></span>
                 <div className="btn_gameplay_home">
                     <button className="btn-home" onClick={upQuizz}>Começar</button>
                 </div>
