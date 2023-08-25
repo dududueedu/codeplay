e48933081a8cf3edb0348ffa859c6041d784ec55
Eduardo Gomes <eduardooffside@gmail.com>
2023-08-24 21:20:47 -0300

    add name game in page one

--- codeplay.client/src/components/PageOne/index.js
+++ codeplay.client/src/components/PageOne/index.js
@@ -6,13 +6,25 @@ import GameSpace from "./../../resources/GameSpace";
 function RenderPage() {
     return (
         <>
-        <div className="master-container">
-            <div className="ff-container">
-            <ProblemCode />
+            <div className="group-code">
+                <span className="code-c">C</span>
+                <span className="code-o">O</span>
+                <span className="code-d">D</span>
+                <span className="code-e">E</span> 
             </div>
-            <GameSpace />
-        </div>
-        <InfoProblem />
+            <div className="master-container">
+                <div className="ff-container">
+                <ProblemCode />
+                </div>
+                <GameSpace />
+            </div>
+            <InfoProblem />
+            <div className="group-right">
+                <span className="code-p">P</span>
+                <span className="code-l">L</span>
+                <span className="code-a">A</span>
+                <span className="code-y">Y</span> 
+            </div>
         </>
     );
 }
